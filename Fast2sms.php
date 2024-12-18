53e25b5ab4e4dbff88ac06155a762bafc92f6b12
Mitesh Viras <87187589+miteshviras@users.noreply.github.com>
2024-12-18 09:59:52 +0530

    Update Fast2sms.php

--- Fast2sms.php
+++ Fast2sms.php
@@ -4,17 +4,29 @@ namespace App\Services\Fast2sms;
 
 use Exception;
 use GuzzleHttp\Client;
-use Illuminate\Support\Facades\Log;
 use GuzzleHttp\Exception\ClientException;
 
 class Fast2sms
 {
+    // Guzzle HTTP client instance
     protected static $client;
+
+    // Initialize the Guzzle HTTP client
     public static function init()
     {
-        self::$client = new Client();
+        if (!self::$client) {
+            self::$client = new Client();
+        }
     }
 
+    /**
+     * Send OTP to a given number using Fast2sms API
+     *
+     * @param string $otp The OTP to send
+     * @param string $number The recipient's phone number
+     * @return array Response indicating success
+     * @throws Exception If there is an error with the request
+     */
     public static function sendOTP($otp, $number)
     {
         try {
@@ -35,8 +47,9 @@ class Fast2sms
                 'success' => 'Message sent successfully'
             ];
         } catch (ClientException $e) {
-            $response = json_decode($e->getResponse()->getBody()->getContents(),true);
-            throw new Exception($response['message'],$response['status_code']) ;
+            // Decode the response body to get the error message and status code
+            $response = json_decode($e->getResponse()->getBody()->getContents(), true);
+            throw new Exception($response['message'], $response['status_code']);
         }
     }
 }
